46633ef07d8d8e777341a430f0a1f0e856120f3c
Shagun <shagunchahar8@gmail.com>
2022-06-08 18:44:11 +0530

    new

--- app/src/main/java/com/example/tictac/MainActivity.kt
+++ app/src/main/java/com/example/tictac/MainActivity.kt
@@ -39,56 +39,78 @@ class MainActivity : AppCompatActivity() {
              {
                  Toast.makeText(this, "CHOOSE ANOTHER BLOCK ", Toast.LENGTH_SHORT).show()
              }
-        var b1=findViewById<Button>(R.id.button1).text.toString()
-        var b2=findViewById<Button>(R.id.button2).text.toString()
-        var b3=findViewById<Button>(R.id.button3).text.toString()
-        var b4=findViewById<Button>(R.id.button4).text.toString()
-        var b5=findViewById<Button>(R.id.button5).text.toString()
-        var b6=findViewById<Button>(R.id.button6).text.toString()
-        var b7=findViewById<Button>(R.id.button7).text.toString()
-        var b8=findViewById<Button>(R.id.button8).text.toString()
-        var b9=findViewById<Button>(R.id.button9).text.toString()
+        val b1=findViewById<Button>(R.id.button1).text.toString()
+        val b2=findViewById<Button>(R.id.button2).text.toString()
+        val b3=findViewById<Button>(R.id.button3).text.toString()
+        val b4=findViewById<Button>(R.id.button4).text.toString()
+        val b5=findViewById<Button>(R.id.button5).text.toString()
+        val b6=findViewById<Button>(R.id.button6).text.toString()
+        val b7=findViewById<Button>(R.id.button7).text.toString()
+        val b8=findViewById<Button>(R.id.button8).text.toString()
+        val b9=findViewById<Button>(R.id.button9).text.toString()
              if(b1==b2&&b2==b3&&b3!="")
              {
                  Toast.makeText(this, "Winner is $b1 ", Toast.LENGTH_LONG).show()
-
+                 newGame()
              }
              else if (b4==b5&&b5==b6&&b6!="")
              {
                  Toast.makeText(this, "Winner is $b4", Toast.LENGTH_LONG).show()
-
+                 newGame()
              }
              else if (b7==b8&&b8==b9&&b9!="")
              {
                  Toast.makeText(this, "Winner is $b7", Toast.LENGTH_LONG).show()
+                 newGame()
              }
              else if (b1==b4&&b4==b7&&b7!="")
              {
                  Toast.makeText(this, "Winner is $b1", Toast.LENGTH_LONG).show()
+                 newGame()
              }
              else if (b2==b5&&b5==b8&&b8!="")
              {
                  Toast.makeText(this, "Winner is $b2", Toast.LENGTH_LONG).show()
+                 newGame()
              }
              else if (b3==b6&&b6==b9&&b9!="")
              {
                  Toast.makeText(this, "Winner is $b3", Toast.LENGTH_LONG).show()
+                 newGame()
              }
              else if (b1==b5&&b5==b9&&b9!="")
              {
                  Toast.makeText(this, "Winner is $b1", Toast.LENGTH_LONG).show()
+                 newGame()
              }
              else if (b3==b5&&b5==b7&&b7!="")
              {
                  Toast.makeText(this, "Winner is $b3", Toast.LENGTH_LONG).show()
+                 newGame()
              }
              else
              {
                  if (count1+count2==9)
                  {
                      Toast.makeText(this, "IT IS A DRAW", Toast.LENGTH_LONG).show()
+                     newGame()
                  }
              }
 
+    }
+    fun newGame()
+    {
+        val b1=findViewById<Button>(R.id.button1).setText("")
+        val b2=findViewById<Button>(R.id.button2).setText("")
+        val b3=findViewById<Button>(R.id.button3).setText("")
+        val b4=findViewById<Button>(R.id.button4).setText("")
+        val b5=findViewById<Button>(R.id.button5).setText("")
+        val b6=findViewById<Button>(R.id.button6).setText("")
+        val b7=findViewById<Button>(R.id.button7).setText("")
+        val b8=findViewById<Button>(R.id.button8).setText("")
+        val b9=findViewById<Button>(R.id.button9).setText("")
+        var chance=1
+        var count1=0
+        var count2=0
     }
 }
