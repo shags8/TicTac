9a5b7064a9ac66e6561967a2116e37f6ab4792b2
Shagun <shagunchahar8@gmail.com>
2022-06-08 12:54:18 +0530

    new

--- app/src/main/java/com/example/tictac/MainActivity.kt
+++ app/src/main/java/com/example/tictac/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.tictac
 import android.bluetooth.BluetoothA2dp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    var chance=1
+    var count1=0
+    var count2=0
 
-
+    fun game(view: View) {
         val B1=findViewById<Button>(R.id.button1)
         val B2=findViewById<Button>(R.id.button2)
         val B3=findViewById<Button>(R.id.button3)
@@ -20,6 +25,16 @@ class MainActivity : AppCompatActivity() {
         val B7=findViewById<Button>(R.id.button7)
         val B8=findViewById<Button>(R.id.button8)
         val B9=findViewById<Button>(R.id.button9)
-
+        val buttonClicked = view as Button
+        if (chance==1)
+        {
+            buttonClicked.text="X"
+            chance=0
+        }
+        else
+        {
+            buttonClicked.text="O"
+            chance=1
+        }
     }
 }
