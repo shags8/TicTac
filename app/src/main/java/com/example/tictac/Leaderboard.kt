ad94f8b625fe1ec6f7fc182f1b1fe6ec5c5d197d
Shagun <shagunchahar8@gmail.com>
2022-06-13 22:39:55 +0530

    new

--- app/src/main/java/com/example/tictac/Leaderboard.kt
+++ app/src/main/java/com/example/tictac/Leaderboard.kt
@@ -3,12 +3,16 @@ package com.example.tictac
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 
 class Leaderboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leaderboard)
         var winners = arrayOfNulls<String>(5)
+        val v1 = findViewById<TextView>(R.id.first)
+        v1.text = ""
+
 
 
 
