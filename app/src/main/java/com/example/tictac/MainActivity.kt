0eab0ab52f7f214ac72cad0d227df089788b6869
Shagun <shagunchahar8@gmail.com>
2022-09-11 12:41:56 +0530

    new

--- app/src/main/java/com/example/tictac/MainActivity.kt
+++ app/src/main/java/com/example/tictac/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +30,10 @@ class MainActivity : AppCompatActivity() {
     {
         val player1 = intent.getStringExtra("player1")
         val player2 = intent.getStringExtra("player2")
+        val playername1 = findViewById<TextView>(R.id.textView5)
+        val playername2 = findViewById<TextView>(R.id.textView6)
+        playername1.text=player1
+        playername2.text=player2
         val buttonClicked = view as Button
         val buttonText = buttonClicked.text.toString()
              if (buttonText=="")
