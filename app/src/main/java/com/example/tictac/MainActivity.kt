d8a2403deb1ff0d93d2eabb39a99c53ead096758
Shagun <shagunchahar8@gmail.com>
2022-10-04 02:23:29 +0530

    new

--- app/src/main/java/com/example/tictac/MainActivity.kt
+++ app/src/main/java/com/example/tictac/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.tictac
 
 import android.annotation.SuppressLint
 import android.app.Dialog
+import android.app.ProgressDialog.show
 import android.bluetooth.BluetoothA2dp
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
@@ -13,6 +15,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -28,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         val intent = intent
         val player1 = intent.getStringExtra("player1")
         val player2 = intent.getStringExtra("player2")
+        var currentplayer = findViewById<TextView>(R.id.textView7)
         val playername1 = findViewById<TextView>(R.id.textView5)
         val playername2 = findViewById<TextView>(R.id.textView6)
         playername1.text = intent.getStringExtra("player1")
@@ -54,21 +58,25 @@ class MainActivity : AppCompatActivity() {
     fun game(view: View) {
         val player1 = intent.getStringExtra("player1")
         val player2 = intent.getStringExtra("player2")
-        val playername3 = findViewById<TextView>(R.id.textView3)
+        var currentplayer = findViewById<TextView>(R.id.textView7)
         val buttonClicked = view as Button
         val buttonTag = buttonClicked.tag.toString().toInt()
-             if (buttonText=="")
+
+             if (filledpos[buttonTag]==-1)
              {
                   if (chance == 1)
                   {
                        buttonClicked.text = "X"
-
+                       currentplayer.text ="PLAYER-1 TURN"
+                       filledpos[buttonTag]=0
                        chance = 0
                        count++
                   }
                   else
                   {
                        buttonClicked.text = "O"
+                       currentplayer.text ="PLAYER-2 TURN"
+                       filledpos[buttonTag]=1
                        chance = 1
                        count++
                   }
@@ -77,105 +85,31 @@ class MainActivity : AppCompatActivity() {
              {
                  Toast.makeText(this, "CHOOSE ANOTHER BLOCK ", Toast.LENGTH_SHORT).show()
              }
+        var winpos = arrayOf(intArrayOf(0,1,2),intArrayOf(3,4,5),intArrayOf(6,7,8),intArrayOf(0,3,6),intArrayOf(1,4,7),
+            intArrayOf(2,5,8),intArrayOf(0,4,8),intArrayOf(2,4,6))
 
-        val b1=findViewById<Button>(R.id.button1).text.toString()
-        val b2=findViewById<Button>(R.id.button2).text.toString()
-        val b3=findViewById<Button>(R.id.button3).text.toString()
-        val b4=findViewById<Button>(R.id.button4).text.toString()
-        val b5=findViewById<Button>(R.id.button5).text.toString()
-        val b6=findViewById<Button>(R.id.button6).text.toString()
-        val b7=findViewById<Button>(R.id.button7).text.toString()
-        val b8=findViewById<Button>(R.id.button8).text.toString()
-        val b9=findViewById<Button>(R.id.button9).text.toString()
-             if(b1==b2&&b2==b3&&b3!="")
-             {
-                 if (b1=="X"){
-                     Toast.makeText(this, "WINNER IS $player1 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }else{
-                     Toast.makeText(this, "WINNER IS $player2 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }
-             }
-             else if (b4==b5&&b5==b6&&b6!="")
-             {
-                 if (b4=="X"){
-                     Toast.makeText(this, "WINNER IS $player1 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }else{
-                     Toast.makeText(this, "WINNER IS $player2 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }
-             }
-             else if (b7==b8&&b8==b9&&b9!="")
-             {
-                 if (b7=="X"){
-                     Toast.makeText(this, "WINNER IS $player1 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }else{
-                     Toast.makeText(this, "WINNER IS $player2 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }
-             }
-             else if (b1==b4&&b4==b7&&b7!="")
-             {
-                 if (b1=="X"){
-                     Toast.makeText(this, "WINNER IS $player1 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }else{
-                     Toast.makeText(this, "WINNER IS $player2 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }
-             }
-             else if (b2==b5&&b5==b8&&b8!="")
-             {
-                 if (b2=="X"){
-                     Toast.makeText(this, "WINNER IS $player1 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }else{
-                     Toast.makeText(this, "WINNER IS $player2 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }
-             }
-             else if (b3==b6&&b6==b9&&b9!="")
-             {
-                 if (b3=="X"){
-                     Toast.makeText(this, "WINNER IS $player1 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }else{
-                     Toast.makeText(this, "WINNER IS $player2 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }
-             }
-             else if (b1==b5&&b5==b9&&b9!="")
-             {
-                 if (b1=="X"){
-                     Toast.makeText(this, "WINNER IS $player1 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }else{
-                     Toast.makeText(this, "WINNER IS $player2 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }
-             }
-             else if (b3==b5&&b5==b7&&b7!="")
-             {
-                 if (b3=="X"){
-                     Toast.makeText(this, "WINNER IS $player1 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }else{
-                     Toast.makeText(this, "WINNER IS $player2 ", Toast.LENGTH_LONG).show()
-                     newGame()
-                 }
-             }
-             else
-             {
-                 if (count==9)
-                 {
-                     Toast.makeText(this, "IT IS A DRAW", Toast.LENGTH_LONG).show()
-                     newGame()
+        for(i in winpos.indices)
+        {
+            var block1 = winpos[i][0]
+            var block2 = winpos[i][1]
+            var block3 = winpos[i][2]
+            if (filledpos[block1]==filledpos[block2]&&filledpos[block2]==filledpos[block3])
+            {
+                if (filledpos[block1]!=-1){
+                    if (filledpos[block1]==0){
+                    AlertDialog.Builder(this).setMessage("PLAYER-1 IS WINNER").setTitle("TIC TAC TOE")
+                        .setPositiveButton("RESTART", DialogInterface.OnClickListener{dialogInterface, i -> newGame() })
+                        .show()
+                    }
+                    else{
+                        AlertDialog.Builder(this).setMessage("PLAYER-2 IS WINNER").setTitle("TIC TAC TOE")
+                            .setPositiveButton("RESTART", DialogInterface.OnClickListener{dialogInterface, i -> newGame() })
+                            .show()
+                    }
 
-                 }
-             }
+                }
+            }
+        }
 
     }
 
@@ -191,6 +125,7 @@ class MainActivity : AppCompatActivity() {
         val b7=findViewById<Button>(R.id.button7).setText("")
         val b8=findViewById<Button>(R.id.button8).setText("")
         val b9=findViewById<Button>(R.id.button9).setText("")
+        filledpos = intArrayOf(-1,-1,-1,-1,-1,-1,-1,-1,-1)
         chance=1
         count=0
     }
@@ -206,6 +141,7 @@ class MainActivity : AppCompatActivity() {
         val b7=findViewById<Button>(R.id.button7).setText("")
         val b8=findViewById<Button>(R.id.button8).setText("")
         val b9=findViewById<Button>(R.id.button9).setText("")
+        filledpos = intArrayOf(-1,-1,-1,-1,-1,-1,-1,-1,-1)
         chance=1
         count=0
     }
